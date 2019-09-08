1c45dff470cb588d45f896cdc8609f9e340117a5
mr625 <52550481+mr625@users.noreply.github.com>
2019-09-07 23:19:01 -0400

    Fixes for deployment

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -6,12 +6,17 @@
 		<h1>{{ error }}</h1>
 		<router-link to="/">Go Back</router-link>
 	</div>
+	<div v-else-if="results.totalresults === 0">
+		<h1>No Results Found</h1>
+		<router-link to="/">Go Back</router-link>
+	</div>
 	<section v-else-if="results" id="results">
 		<ol id="resultsList">
 			<li v-for="result in results" :key="result.p_id">
 				<Result :result="result"/>
 			</li>
 		</ol>
+		<router-link to="/">Go Back</router-link>
 	</section>
 </template>
 
@@ -37,7 +42,7 @@ export default {
 			const resp = await axios.get(
 				`/api/v1/results/${this.$route.params.platform}/${this.$route.params.gamertag}`
 			);
-			this.results = resp.data.results;
+			this.results = resp.data.results || resp.data;
 			this.loading = false;
 		} catch (err) {
 			this.loading = false;
--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const dotenv = require('dotenv');
 // Load env (allows us to use environment variables anywhere in our application)
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 8000;
 const app = express();
+
+// Dev logging
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 // Profile routes
 app.use('/api/v1/profile', require('./routes/profile'));
@@ -14,15 +18,13 @@ app.use('/api/v1/results', require('./routes/results'));
 
 if (process.env.NODE_ENV === 'production') {
 	// Static folder
-	app.use(express.static(__dirname, '/public/'));
+	app.use(express.static(__dirname + '/public'));
 	// SPA Handling
 	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-// Dev logging
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
-}
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}.`);
